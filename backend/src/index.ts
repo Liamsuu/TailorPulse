d69e981105f1940be149f4e4b62c549440a4bfe8
Liam <liamsuu14974@gmail.com>
2026-05-10 20:22:51 +0100

    Add API rate limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
 import express from "express";
 import apiRouter from "./routes/apiRoutes.js";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    message:
+      "Too many CVs generated. Take a breather and try again in 15 minutes!",
+  },
+});
+
 app.use(cors({ exposedHeaders: ["Content-Disposition"] }));
 app.use(express.json());
+app.use(limiter);
+app.set("trust proxy", 1);
+
 const PORT: number = 3000;
 
 app.use("/api", apiRouter);
